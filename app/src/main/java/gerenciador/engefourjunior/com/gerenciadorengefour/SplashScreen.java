7568c9d4b2fa8bd7b9f4ea3ccaa90f270a292872
Leo <fuchs_leo@hotmail.com>
2017-05-09 14:07:24 -0400

    Splash Screen OK!

--- app/src/main/java/gerenciador/engefourjunior/com/gerenciadorengefour/SplashScreen.java
+++ app/src/main/java/gerenciador/engefourjunior/com/gerenciadorengefour/SplashScreen.java
@@ -3,24 +3,28 @@ package gerenciador.engefourjunior.com.gerenciadorengefour;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class SplashScreen extends Activity {
 
-    private static int SPLASH_TIME_OUT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable(){
+        new Timer().schedule(new TimerTask()
+        {
             @Override
             public void run() {
+                finish();
+
                 Intent it = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(it);
-                finish();
+
             }
-        },SPLASH_TIME_OUT);
+        }, 3000);
     }
 }
